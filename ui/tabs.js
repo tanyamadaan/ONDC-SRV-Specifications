ff7e4876fe30c1efbe0d83fb2a38b4d56bc8050d
Tanya Madaan <tanyamadaan@Tanyas-MacBook-Air.local>
2024-02-01 15:41:18 +0530

    bug fix

--- ui/tabs.js
+++ ui/tabs.js
@@ -1,51 +1,50 @@
 // tabs.js
 
 function onFirstLoad(build_spec, features) {
-      let data = build_spec;
-    
-      try {
-        initSchema(data["x-enum"]);
-      } catch (error) {
-        console.trace(error);
-      }
-    
-      try {
-        initTag(data["x-tags"]);
-      } catch (error) {
-        console.trace(error);
-      }
-    
-      try {
-        loadExample(data["x-examples"]);
-      } catch (error) {
-        console.trace(error);
-      }
-    
-      try {
-        loadFeatures(features);
-      } catch (error) {
-        console.trace(error);
-      }
-    
-      try {
-        loadFlows(data["x-flows"]);
-      } catch (error) {
-        console.trace(error);
-      }
-    
-      try {
-        loadAttributes(data["x-attributes"]);
-      } catch (error) {
-        console.trace(error);
-      }
-      try {
-        loadErrors(data["x-errorcodes"]);
-      } catch (error) {
-        console.trace(error);
-      }
-    }
-    
-    // window.onload = function(){
-    //       onFirstLoad(build_spec)
-    // }
-    
+  let data = build_spec;
+
+  try {
+    initSchema(data["x-enum"]);
+  } catch (error) {
+    console.trace(error);
+  }
+
+  try {
+    initTag(data["x-tags"]);
+  } catch (error) {
+    console.trace(error);
+  }
+
+  try {
+    loadExample(data["x-examples"]);
+  } catch (error) {
+    console.trace(error);
+  }
+
+  try {
+    loadFeatures(features);
+  } catch (error) {
+    console.trace(error);
+  }
+
+  try {
+    loadFlows(data["x-flows"]);
+  } catch (error) {
+    console.trace(error);
+  }
+
+  try {
+    loadAttributes(data["x-attributes"]);
+  } catch (error) {
+    console.trace(error);
+  }
+  try {
+    loadErrors(data["x-errorcodes"]);
+  } catch (error) {
+    console.trace(error);
+  }
+}
+
+// window.onload = function(){
+//       onFirstLoad(build_spec)
+// }
